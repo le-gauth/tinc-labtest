c4acd31a62d985960a506996561c43a5b68d5625
le-gauth <gavellan@protonmail.com>
2025-06-09 18:33:19 +0200

    modify: pour générer les clés

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -e
 
-exec tincd -n netlabssir -D -d3
+NETNAME="netlabssir"
+CONF_DIR="/etc/tinc/$NETNAME"
+
+# Si la clé n'existe pas, on la génère automatiquement
+if [ ! -f "$CONF_DIR/rsa_key.priv" ]; then
+  echo "\n Aucune clé trouvée, génération automatique de la clé Tinc : \n \n"
+  tincd -n "$NETNAME" -K4096 <<< ""
+fi
+
+exec tincd -n "$NETNAME" -D -d3
